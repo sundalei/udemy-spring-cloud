9ae719e4d1fdc5fc7b06145b7ab3532e3d06876f
Leo Sun <sundalei1988@gmail.com>
2022-06-14 22:10:34 +0800

    add api gateway

---
+++ api-gateway-server/src/main/java/com/in28minutes/microservices/netflixzuulapigatewayserver/ApiGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.netflixzuulapigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayServerApplication.class, args);
+	}
+
+}
