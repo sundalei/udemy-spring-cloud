ef642c47c7f617cbc736ad37620c22e20db0b307
Leo Sun <sundalei1988@gmail.com>
2022-06-15 09:23:32 +0800

    add gateway routelocator

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/ExchangeValueNotFoundException.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/ExchangeValueNotFoundException.java
@@ -2,7 +2,9 @@ package com.in28minutes.microservices.currencyexchangeservice;
 
 public class ExchangeValueNotFoundException extends RuntimeException {
 
-    public ExchangeValueNotFoundException(String message) {
+    private static final long serialVersionUID = 1L;
+
+	public ExchangeValueNotFoundException(String message) {
         super(message);
     }
 }
